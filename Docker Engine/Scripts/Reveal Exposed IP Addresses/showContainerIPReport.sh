82ba8a96f08060ed03855306405c535e05497668
Ashwin Randhir <200tristan@gmail.com>
2023-02-17 13:11:54 -0500

    Update showContainerIPReport.sh
    +Formatting

--- Docker Engine/Scripts/Reveal Exposed IP Addresses/showContainerIPReport.sh
+++ Docker Engine/Scripts/Reveal Exposed IP Addresses/showContainerIPReport.sh
@@ -3,11 +3,15 @@
 # Get list of all running docker containers
 container_list=$(docker ps --format "{{.Names}}")
 
+# Set column headers for report
+printf "%-20s %-20s %-20s\n" "Container Name" "Public IP" "Private IP" > report.txt
+
 # Loop through each container and run "curl www.ifconfig.me"
 for container_name in $container_list
 do
-    echo "Running curl command in container: $container_name"
-    docker exec $container_name curl www.ifconfig.me
-done > report.txt
+    public_ip=$(docker exec $container_name curl -s www.ifconfig.me)
+    private_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $container_name)
+    printf "%-20s %-20s %-20s\n" "$container_name" "$public_ip" "$private_ip" >> report.txt
+done
 
 echo "Job Complete: Report generated in report.txt"
